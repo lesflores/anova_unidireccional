dc5d06cd6f4dbc91618e3832e10e7f6fa1297fa6
Lesly Estefanía Flores Rivera <147342430+lesflores@users.noreply.github.com>
2024-11-07 21:38:48 -0600

    Add files via upload

---
+++ anova_unidireccional.R
@@ -0,0 +1,24 @@
+#------------- ANOVA
+# La prueba ANOVA unidireccional se usa para comparar la media de dos o más grupos de una variable.
+# En este caso, la aplicamos para ver si hay diferencias significativas en la satisfacción ciudadana 
+# entre distintos programas sociales (educación, seguridad, salud e infraestructura).
+# El programa social es la variable independiente y la satisfacción ciudadana es la variable dependiente.
+
+
+#------------- Librerías
+library(readxl) # Para leer datos en formato Excel
+
+#------------- Cargar la base de datos, disponible para descargar aquí:__
+data <- read_excel("datos_programa.xlsx")
+
+# Visualizar las primeras filas
+head(data)
+
+#------------- ANOVA
+anova_programa <- aov(satisfaccion ~ programa, data = data)
+
+# Resultados del ANOVA
+summary(anova_programa)
+
+# Prueba de Tukey para identificar en qué grupos existen diferencias significativas
+TukeyHSD(anova_programa)
